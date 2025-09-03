589a9d340eab308b3c49909c5d5f93187271a67c
Utsav <utsav7196@gmail.com>
2025-09-03 05:06:40 +0000

    Migration logic

--- src/migration/1756718266340-InitialMigration.ts
+++ src/migration/1756718266340-InitialMigration.ts
@@ -5,11 +5,27 @@ export class InitialMigration1756718266340 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "UserDetails" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_5dcb82aacc9a5256f29610285c7" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "UserDetails" (
+         "id" SERIAL NOT NULL,
+         "email" character varying NOT NULL,
+         "password" character varying NOT NULL,
+         CONSTRAINT "PK_UserDetails_id" PRIMARY KEY ("id")
+       )`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "Projects" (
+         "id" SERIAL NOT NULL,
+         "name" character varying NOT NULL,
+         "description" text NOT NULL,
+         "userId" integer NOT NULL,
+         CONSTRAINT "FK_Projects_userId" FOREIGN KEY ("userId") REFERENCES "UserDetails"("id") ON DELETE CASCADE,
+         CONSTRAINT "PK_Projects_id" PRIMARY KEY ("id")
+       )`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "Projects"`);
     await queryRunner.query(`DROP TABLE "UserDetails"`);
   }
 }
